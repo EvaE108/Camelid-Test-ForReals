7b4f80e5bf0fd3394ffe4f6bc818fddfdc830370
EvaE108 <144815601+EvaE108@users.noreply.github.com>
2025-09-25 15:52:55 -0400

    Create Camelid.js

---
+++ js/classes/models/Camelid.js
@@ -0,0 +1,14 @@
+import {Model} from "./models.js";
+
+let bonesList = [
+    "Organs/bread pack"
+];
+
+export default new Model(
+    "Camelid",
+    bonesList,
+    11,
+    [0, 10, -1],
+    "This is a model of Camelid",
+    "/img/models/preview/camelid.png"
+);
