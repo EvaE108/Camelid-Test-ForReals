eb0ed229c11094440e32124e97db852208f1cb74
EvaE108 <144815601+EvaE108@users.noreply.github.com>
2025-10-06 15:15:55 -0400

    Update and rename Camelid_Inside to Camelid_Inside.js

---
+++ js/classes/models/Camelid_Inside.js
@@ -0,0 +1,15 @@
+import {Model} from "./models.js";
+
+let bonesList = [
+    "Stuff/guts",
+    "Stuff/extra"
+];
+
+export default new Model(
+    "Camelid_Inside",
+    bonesList,
+    11,
+    [0, 10, -1],
+    "",
+    ""
+);
